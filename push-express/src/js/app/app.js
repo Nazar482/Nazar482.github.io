ce1382f82bc9fdd0e38d6a6c511bb9004ace2b74
Nazar482 <atamanyuk.nazar48@gmail.com>
2017-09-11 02:10:41 +0300

    initial update pushexpress

--- push-express/src/js/app/app.js
+++ push-express/src/js/app/app.js
@@ -13,19 +13,6 @@ $(document).ready(function() {
         cssEase: 'linear',
         centerMode: true,
     });
-    $('#fullpage').fullpage({
-        menu: '.menu',
-        anchors:['about', 'services', 'tech', 'works', 'contacts'],
-        navigation: true,
-        navigationPosition: 'left',
-        keyboardScrolling: true,
-        scrollHorizontally: true,
-        fadingEffect: true,
-        verticalCentered: true,
-        css3: true,
-        scrollingSpeed: 500,
-        fitToSection: false
-    });
 
     $(".share-button").click(function(){
         $(".share-block").toggleClass("hide");
@@ -59,4 +46,53 @@ $(document).ready(function() {
         $(".form__success").fadeIn(1000).delay(3000).fadeOut(1000);
         $(".section__main-content").fadeOut().delay(4500).fadeIn(1000);
     })
+
+
+});
+
+// Cache selectors
+
+var lastId,
+    sideMenu = $("#side-menu"),
+    sideMenuHeight = 200;
+    // All list items
+    menuItems = sideMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+    var href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top-sideMenuHeight+1;
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+    }, 300);
+    e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+    // Get container scroll position
+    var fromTop = $(this).scrollTop()+sideMenuHeight + 60;
+
+    // Get id of current scroll item
+    var cur = scrollItems.map(function(){
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length-1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+        lastId = id;
+        // Set/remove active class
+        menuItems
+            .parent().removeClass("active")
+            .end().filter("[href='#"+id+"']").parent().addClass("active");
+    }
 });
