5e5edb105ca4e92a23cc5c53420199a0bde4a143
Nazar482 <atamanyuk.nazar48@gmail.com>
2017-09-10 21:53:26 +0300

    update

---
+++ push-express/src/js/app/app.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+
+    $(".loader").css("zoom","150%").fadeOut(5000, function() {
+        $(".wrapper").fadeIn(1000);
+    });
+
+    $('.gallery').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        centerMode: true,
+    });
+    $('#fullpage').fullpage({
+        menu: '.menu',
+        anchors:['about', 'services', 'tech', 'works', 'contacts'],
+        navigation: true,
+        navigationPosition: 'left',
+        keyboardScrolling: true,
+        scrollHorizontally: true,
+        fadingEffect: true,
+        verticalCentered: true,
+        css3: true,
+        scrollingSpeed: 500,
+        fitToSection: false
+    });
+
+    $(".share-button").click(function(){
+        $(".share-block").toggleClass("hide");
+        $(".share-button").children("i").toggleClass("icon-share icon-close");
+    });
+
+
+    $(".menu-toggle").click(function(){
+        $(".menu").show();
+    });
+    $(".close-menu").click(function(){
+        $(".menu").hide();
+    });
+    $(".nav__link").click(function(){
+        if($(window).width() < 900){
+            $(".menu").hide();
+        }
+
+    });
+
+    $(".modal").fadeOut();
+    $(".view-gallery").click(function(){
+        $(window).trigger('resize');
+        $(".modal").fadeIn(1000);
+    });
+    $(".close-modal").click(function(){
+        $(".modal").fadeOut();
+    });
+
+    $(".button-submit").click(function(){
+        $(".form__success").fadeIn(1000).delay(3000).fadeOut(1000);
+        $(".section__main-content").fadeOut().delay(4500).fadeIn(1000);
+    })
+});
---
+++ ubt/src/js/main.js
@@ -0,0 +1,19 @@
+/*
+ * Jquery
+ */
+//= ../../node_modules/jquery/dist/jquery.js
+//= ../../node_modules/slick-carousel/slick/slick.js
+/*
+ * FullpageJS
+ */
+//= plugins/jquery.fullpage.js
+
+
+/*
+
+
+
+/*
+ * Custom scripts
+ */
+//= app/app.js
